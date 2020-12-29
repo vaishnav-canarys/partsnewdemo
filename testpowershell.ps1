06e29e45047354f7d4acf405be72d2300a11b01f
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2020-12-29 15:59:57 +0530

    Update testpowershell.ps1

--- testpowershell.ps1
+++ testpowershell.ps1
@@ -1,1 +1,21 @@
-Write-Host "Test Output"
+$pool_name = "TestPool"
+$site_name = "TestSite"
+$packagepath = "C:\Temp"
+
+if(!(Test-Path IIS:\AppPools\$pool_name ))
+{
+    #create the app pool
+    New-WebAppPool -Name $pool_name -Force
+}
+else {
+    echo "Pool exists"
+}
+
+#check if the site exists
+if(!(Test-Path IIS:\Sites\$site_name  ))
+{
+   New-Website -Name $site_name -ApplicationPool $pool_name -Force -PhysicalPath $packagepath -Port 8083
+}
+else {
+    echo "site exists"
+}
